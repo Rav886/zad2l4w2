58116066ddbe6c0548dd701b586a0c55f35e2cf8
Ra v <timon521182@hotmail.com>
2023-02-12 20:42:20 +0100

    Add project files.

---
+++ zad2l4w2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace zad2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj pierwszą litere");
+
+            string first = Console.ReadLine();
+            char.Parse(first);
+
+            Console.WriteLine("Podaj drugą litere");
+
+            string secound = Console.ReadLine();
+            char.Parse(secound);
+
+
+            Console.WriteLine("Podaj trzecią litere");
+
+            string third = Console.ReadLine();
+            char.Parse(third);
+
+            Console.WriteLine($"Podane litery w odwrotnej kolejności: {third}, {secound}, {first} ");
+        }
+    }
+}
